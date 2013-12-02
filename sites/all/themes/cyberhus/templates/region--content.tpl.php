2b8b115e51020c7a5be7b1b3aa402a3c7141de60
Michael Lajlev <lajlev@gmail.com>
2013-12-02 20:28:39 +0100

    sidebar second added

--- sites/all/themes/cyberhus/templates/region--content.tpl.php
+++ sites/all/themes/cyberhus/templates/region--content.tpl.php
@@ -8,7 +8,7 @@
     <h1 class="title" id="page-title"><?php print $title; ?></h1>
     <?php if ($title_hidden): ?></div><?php endif; ?>
     <?php endif; ?>
-    <?php print render($title_suffix); ?>    
+    <?php print render($title_suffix); ?>
     <?php if ($tabs && !empty($tabs['#primary'])): ?><div class="tabs clearfix"><?php print render($tabs); ?></div><?php endif; ?>
     <?php if ($action_links): ?><ul class="action-links"><?php print render($action_links); ?></ul><?php endif; ?>
     <?php print $content; ?>
@@ -16,4 +16,3 @@
   </div>
 
 </div>
-  <div class="content_bottom"></div>
