502aed4e60058e66e764f0fc3f437031b4d98564
Kevin Andries <andrieskevin5320@gmail.com>
2014-01-23 10:04:26 +0100

    add custom body secret grid template

---
+++ sites/all/themes/cyberhus/templates/views-view-grid--body-secrets.tpl.php
@@ -0,0 +1,27 @@
+<?php
+
+/*
+ *  Template for Body Secrets grid
+ *  
+ */
+
+    if (!empty($title)) : ?>
+  <h3><?php print $title; ?></h3>
+<?php endif; ?>
+<table cellspacing="10" class="<?php print $class; ?>"<?php print $attributes; ?>>
+  <?php if (!empty($caption)) : ?>
+    <caption><?php print $caption; ?></caption>
+  <?php endif; ?>
+
+  <tbody>
+    <?php foreach ($rows as $row_number => $columns): ?>
+      <tr <?php if ($row_classes[$row_number]) { print 'class="' . $row_classes[$row_number] .'"';  } ?>>
+        <?php foreach ($columns as $column_number => $item): ?>
+          <td <?php if ($column_classes[$row_number][$column_number]) { print 'class="' . $column_classes[$row_number][$column_number] .'"';  } ?>>
+            <?php print $item; ?>
+          </td>
+        <?php endforeach; ?>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>
