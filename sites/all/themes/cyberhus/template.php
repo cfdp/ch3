eb781e451616cf5fa8f95a50ce6608307bb42381
Michael Lajlev <lajlev@gmail.com>
2014-01-24 08:24:54 +0100

    general style changes

--- sites/all/themes/cyberhus/template.php
+++ sites/all/themes/cyberhus/template.php
@@ -5,12 +5,12 @@
  * This file is empty by default because the base theme chain (Alpha & Omega) provides
  * all the basic functionality. However, in case you wish to customize the output that Drupal
  * generates through Alpha & Omega this file is a good place to do so.
- * 
+ *
  * Alpha comes with a neat solution for keeping this file as clean as possible while the code
  * for your subtheme grows. Please read the README.txt in the /preprocess and /process subfolders
  * for more information on this topic.
  */
- 
+
  /**
  * Output breadcrumb as an unorderd list with unique and first/last classes
  */
@@ -23,7 +23,7 @@ function cyberhus_breadcrumb($variables) {
     $crumbs = '<ul class="breadcrumbs clearfix">';
     $array_size = count($breadcrumb);
     $i = 0;
-    
+
     // Fjern elementer i breadcrumb på Node forms
     if(arg(0)=="node" && arg(1)=="add") {
     while ( $i < 1) {
@@ -36,7 +36,7 @@ function cyberhus_breadcrumb($variables) {
     }
     $crumbs .= "<li class='last'>".drupal_get_title()."</li>";
     $crumbs .= '</ul>';
-    return $crumbs;    
+    return $crumbs;
     }
     // Fjern elementer i breadcrumbs på Søgesiden
     if(arg(0)=="search" && arg(1)=="node") {
@@ -50,7 +50,7 @@ function cyberhus_breadcrumb($variables) {
     }
     $crumbs .= "<li class='last'>".drupal_get_title()."</li>";
     $crumbs .= '</ul>';
-    return $crumbs;    
+    return $crumbs;
     }
     else {
     while ( $i < $array_size) {
@@ -71,7 +71,7 @@ function cyberhus_breadcrumb($variables) {
 /**
  * SEARCH BLOCK
  */
- 
+
 // Add some cool text to the search block form
 function cyberhus_form_alter(&$form, &$form_state, $form_id) {
   if ($form_id == 'search_block_form') {
@@ -90,16 +90,16 @@ function cyberhus_textarea($variables) {
   $element['#attributes']['cols'] = $element['#cols'];
   $element['#attributes']['rows'] = 8;
   _form_set_class($element, array('form-textarea'));
- 
+
   $wrapper_attributes = array(
     'class' => array('form-textarea-wrapper'),
   );
- 
+
   // Add resizable behavior.
   if (!empty($element['#resizable'])) {
     $wrapper_attributes['class'][] = 'resizable';
   }
- 
+
   $output = '<div' . drupal_attributes($wrapper_attributes) . '>';
   $output .= '<textarea' . drupal_attributes($element['#attributes']) . '>' . check_plain($element['#value']) . '</textarea>';
   $output .= '</div>';
