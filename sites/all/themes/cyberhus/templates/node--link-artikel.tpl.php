b2df7f573d4bc9b4caeaa9e2841bc53c8a5a9295
DanielMM <moisdaniel7@gmail.com>
2014-05-07 10:22:38 +0200

    Restore title to the link-article template.

---
+++ sites/all/themes/cyberhus/templates/node--link-artikel.tpl.php
@@ -0,0 +1,31 @@
+<article<?php print $attributes; ?>>
+
+<?php print render($title_prefix); ?>
+<header>
+  <h2<?php print $title_attributes; ?>><?php print $title ?></h2>
+</header>
+<?php print render($title_suffix); ?>
+
+<?php 
+	$tid=$node->field_tema_kategori['und'][0]['tid']; 
+	$term = taxonomy_term_load($tid);
+	$kategori_name = url("temaer/".$term->name);
+	$kategori_name = str_replace('%20', '-', $kategori_name);
+?>
+<p><a href="<?php echo $kategori_name; ?>" class="btn">Se alle artikler på <?php echo strtolower($term->name); ?></a></p>
+<div<?php print $content_attributes; ?>>
+<?php
+    // We hide the comments and links now so that we can render them later.
+	hide($content['comments']);
+	hide($content['links']);
+	print render($content['body']);
+?>
+</div>
+<div class="clearfix">
+	<?php if (!empty($content['links'])): ?>
+		<nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
+	<?php endif; ?>
+
+	<?php print render($content['comments']); ?>
+</div>
+</article>
