a64249601d68c5bea128aa0ae6f4b06a196348a5
Benjamin Christensen <benjamin@greenvolcano.dk>
2015-11-03 06:26:30 +0100

    Moving related articles button to bottom of page and checking for term before printing

--- sites/all/themes/cyberhus/templates/node--link-artikel.tpl.php
+++ sites/all/themes/cyberhus/templates/node--link-artikel.tpl.php
@@ -5,14 +5,6 @@
   <h2<?php print $title_attributes; ?>><?php print $title ?></h2>
 </header>
 <?php print render($title_suffix); ?>
-
-<?php 
-	$tid=$node->field_tema_kategori['und'][0]['tid']; 
-	$term = taxonomy_term_load($tid);
-	$kategori_name = url("temaer/".$term->name);
-	$kategori_name = str_replace('%20', '-', $kategori_name);
-?>
-<p><a href="<?php echo $kategori_name; ?>" class="btn">Se alle artikler på <?php echo strtolower($term->name); ?></a></p>
 <div<?php print $content_attributes; ?>>
 <?php
     // We hide the comments and links now so that we can render them later.
@@ -25,7 +17,16 @@
 	<?php if (!empty($content['links'])): ?>
 		<nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
 	<?php endif; ?>
-
+  <?php
+    /* Print related articles button */
+    if (isset($node) && isset($node->field_tema_kategori['und'][0])) {
+      $tid=$node->field_tema_kategori['und'][0]['tid'];
+      $term = taxonomy_term_load($tid);
+      $kategori_name = url("temaer/".$term->name);
+      $kategori_name = str_replace('%20', '-', $kategori_name);
+      print '<p><a href="' . $kategori_name . '" class="btn">Se alle artikler om ' . strtolower($term->name) . '</a></p>';
+    }
+  ?>
 	<?php print render($content['comments']); ?>
 </div>
 </article>
