177e07fe41461cbbcdb555fc35bd78ac1d93cdfc
Benjamin Christensen <benjamin@greenvolcano.dk>
2016-03-07 09:35:25 +0100

    Implement improved multi chat widget
    Reflects the states of the embedded chat services

--- sites/all/themes/cyberhus/chat-multi-widget/js/calculate_state.js
+++ sites/all/themes/cyberhus/chat-multi-widget/js/calculate_state.js
@@ -1,26 +1,57 @@
 /**
- * Receive activate or deactive messages from the iframe
+ * This script makes the wrapper multi widget reflect the states of the embedded
+ * chat widgets.
+ *
  * https://developer.mozilla.org/en-US/docs/Web/API/Window/postMessage
  */
 (function ($) {
-    window.addEventListener("message", receiveMessage, false);
-    var opekaMultiWidgetState = "pending";
-    function receiveMessage(event) {
-      if (event.origin !== "https://dev.demo") {
-        console.log("Bad window");
-        return;
-      }
-      else {
-        calculateWidgetState(event.data);
-      }
-    }
-    
-    function calculateWidgetState(stateShift) {
-      if ( event.data == "Activate") {
-        if (opekaMultiWidgetState == "pending" || opekaMultiWidgetState == "closed" ) {
-          opekaMultiWidgetState = "available";
+  var chatStates = {}, // Object holding the state of the embedded chat services
+    validOrigins = ['https://aarhus.curachat.com', 'https://kbh.curachat.com', 'https://rksk.curachat.com'],
+    opekaMultiWidgetState = 'chat-busy';
+
+  function searchObject(needle) {
+    for (var prop in chatStates) {
+      if (chatStates.hasOwnProperty(prop)) {
+        if (chatStates[prop] === needle) {
+          return true;
+        }
+        else {
+          return false;
         }
       }
     }
+  }
 
+  function calculateMultiWidgetState() {
+    if (searchObject('Open')) {
+      // We have an active chat
+      opekaMultiWidgetState = 'chat-open';
+
+    } else if (searchObject('Occupied')) {
+      // We have occupied chats
+      opekaMultiWidgetState = 'chat-busy';
+    } else {
+      // All chats are closed
+      opekaMultiWidgetState = 'chat-closed';
+    }
+    $('body').removeClass('chat-closed chat-busy chat-open').addClass(opekaMultiWidgetState);
+  }
+
+  /**
+   * Recieves status messages from the embedded chat services
+   * @param {Object} event
+   */
+  function receiveMessage(event) {
+    if (validOrigins.indexOf(event.origin) !== -1) {
+      // Update status of the messaging chat
+      chatStates[event.origin] = event.data;
+      calculateMultiWidgetState();
+    } else {
+      // The origin is not valid
+      console.log("Bad window");
+      return;
+    }
+  }
+
+  window.addEventListener("message", receiveMessage, false);
 }(jQuery));
