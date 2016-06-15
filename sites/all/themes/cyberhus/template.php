3e4566035cf852eebde51fbd02c82ff48e4de6b0
Benjamin Christensen <benjamin@greenvolcano.dk>
2016-06-15 21:36:34 +0200

    Deleting debug msg

--- sites/all/themes/cyberhus/template.php
+++ sites/all/themes/cyberhus/template.php
@@ -224,8 +224,6 @@ function cyberhus_menu_link__menu_top_menu($variables) {
 function cyberhus_menu_local_action($variables) {
   $link = $variables['element']['#link'];
   // Remove add new forum topic from /forum page
-  $path = current_path();
-  dpm(arg(0), substr($link['href'],0, 14));
   if (arg(0) == 'forum' &&  substr($link['href'], 0, 14) == 'node/add/forum') {
     $output = null;
   }
