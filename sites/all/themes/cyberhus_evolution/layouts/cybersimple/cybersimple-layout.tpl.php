dba3d27e772cda7f06ec69bccbb0be20990e1af9
Benjamin Christensen <benjamin@greenvolcano.dk>
2016-06-28 15:18:08 +0200

    Styling for cyberlike, footer and forum

--- sites/all/themes/cyberhus_evolution/layouts/cybersimple/cybersimple-layout.tpl.php
+++ sites/all/themes/cyberhus_evolution/layouts/cybersimple/cybersimple-layout.tpl.php
@@ -24,33 +24,36 @@
     <?php print render($page['navigation']); ?>
   </header>
 
-  <div class="l-main">
-    <div class="l-content" role="main">
-      <?php print render($page['highlighted']); ?>
-      <?php print $breadcrumb; ?>
-      <a id="main-content"></a>
-      <?php print render($title_prefix); ?>
-      <?php if ($title): ?>
-        <h1><?php print $title; ?></h1>
-      <?php endif; ?>
-      <?php print render($title_suffix); ?>
-      <?php print $messages; ?>
-      <?php print render($tabs); ?>
-      <?php print render($page['help']); ?>
-      <?php if ($action_links): ?>
-        <ul class="action-links"><?php print render($action_links); ?></ul>
-      <?php endif; ?>
-      <?php print render($page['content']); ?>
-      <?php print $feed_icons; ?>
-    </div>
+  <div class="l-main--wrapper">
+    <div class="l-main">
+      <div class="l-content" role="main">
+        <?php print render($page['highlighted']); ?>
+        <?php print $breadcrumb; ?>
+        <a id="main-content"></a>
+        <?php print render($title_prefix); ?>
+        <?php if ($title): ?>
+          <h1><?php print $title; ?></h1>
+        <?php endif; ?>
+        <?php print render($title_suffix); ?>
+        <?php print $messages; ?>
+        <?php print render($tabs); ?>
+        <?php print render($page['help']); ?>
+        <?php if ($action_links): ?>
+          <ul class="action-links"><?php print render($action_links); ?></ul>
+        <?php endif; ?>
+        <?php print render($page['content']); ?>
+        <?php print $feed_icons; ?>
+      </div>
 
-    <?php /* print render($page['sidebar_first']); */ ?>
-    <?php /* print render($page['sidebar_second']); */ ?>
+      <?php /* print render($page['sidebar_first']); */ ?>
+      <?php /* print render($page['sidebar_second']); */ ?>
+    </div>
   </div>
 
-  <footer class="l-footer" role="contentinfo">
-    <div class="footer-wrapper">
-      <?php print render($page['footer']); ?>
-    </div>
-  </footer>
+  <div class="l-footer--wrapper">
+    <footer class="l-footer" role="contentinfo">
+        <?php print render($page['footer']); ?>
+    </footer>
+  </div>
+
 </div>
