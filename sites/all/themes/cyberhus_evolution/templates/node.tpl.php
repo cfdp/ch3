31e836da2a89a99c76244ef1e361d1304cf1e502
Benjamin Christensen <benjamin@greenvolcano.dk>
2016-07-10 21:39:16 +0200

    Print comment form in node view

--- sites/all/themes/cyberhus_evolution/templates/node.tpl.php
+++ sites/all/themes/cyberhus_evolution/templates/node.tpl.php
@@ -130,6 +130,8 @@
     $block = _block_get_renderable_array(_block_render_blocks(array($blockObject)));
     $output = drupal_render($block);
     print $output;
+    /* Print comment form */
+    print drupal_render(drupal_get_form("comment_node_{$node->type}_form", (object) array('nid' => $node->nid)));
 
   ?>
 
