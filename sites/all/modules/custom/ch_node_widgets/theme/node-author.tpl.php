b6269824e352a206c0cdf2b3aa39b5395f7ecba4
Henrik Bak Thomassen <hbt@peytz.dk>
2017-10-06 13:52:41 +0200

    Author box tweak

--- sites/all/modules/custom/ch_node_widgets/theme/node-author.tpl.php
+++ sites/all/modules/custom/ch_node_widgets/theme/node-author.tpl.php
@@ -3,31 +3,46 @@
 ?>
 
 <div class="author-box">
-  <div class="author-avatar"></div>
-  <div class="author-type"><?php print $type_label['singular'] . " " . t('by') ?></div>
-  <div class="author-name">
-    <?php
-    if(isset($node->field_navn[LANGUAGE_NONE])) {
-      print $node->field_navn[LANGUAGE_NONE][0]['value'];
-    }
-    else {
-      print t("Anonymous");
-    }
-    ?>
+  <div class="author-first">
+    <div class="author-avatar">
+      <?php
+      if(isset($node->field_avatar[LANGUAGE_NONE])) {
+        print cyberhus_clean_term_display($node->field_avatar[LANGUAGE_NONE][0]['target_id']);
+      }
+      // Default
+      else {
+        $terms = taxonomy_get_term_by_name('default', 'avatars');
+        print cyberhus_clean_term_display(key($terms));
+      }
+      ?>
+    </div>
   </div>
-  <div class="author-gender">
-    <?php
-    if(isset($node->field_brevk_koen[LANGUAGE_NONE])) {
-      print cyberhus_clean_term_display($node->field_brevk_koen[LANGUAGE_NONE][0]['tid']);
-    }
-    ?>
+  <div class="author-second">
+    <div class="author-type"><?php print $type_label['singular'] . " " . t('by') ?></div>
+    <div class="author-name">
+      <?php
+      if(isset($node->field_navn[LANGUAGE_NONE])) {
+        print $node->field_navn[LANGUAGE_NONE][0]['value'];
+      }
+      else {
+        print t("Anonymous");
+      }
+      ?>
+    </div>
+    <div class="author-gender">
+      <?php
+      if(isset($node->field_brevk_koen[LANGUAGE_NONE])) {
+        print cyberhus_clean_term_display($node->field_brevk_koen[LANGUAGE_NONE][0]['tid']);
+      }
+      ?>
+    </div>
+    <div class="author-age">
+      <?php
+      if(isset($node->field_brevk_alder[LANGUAGE_NONE])) {
+        print cyberhus_clean_term_display($node->field_brevk_alder[LANGUAGE_NONE][0]['tid']);
+      }
+      ?>
+    </div>
+    <div class="author-created"><?php print t('Created !interval ago', array('!interval' => format_interval(time() - $node->created))); ?></div>
   </div>
-  <div class="author-age">
-    <?php
-    if(isset($node->field_brevk_alder[LANGUAGE_NONE])) {
-      print cyberhus_clean_term_display($node->field_brevk_alder[LANGUAGE_NONE][0]['tid']);
-    }
-    ?>
-  </div>
-  <div class="author-created"><?php print t('Created !interval ago', array('!interval' => format_interval(time() - $node->created))); ?></div>
 </div>
