30991d49f74845cd566e543ac4f9a52b0e2e04e9
Henrik Bak Thomassen <hbt@peytz.dk>
2017-10-11 13:11:43 +0200

    Features updates + styling

--- sites/all/themes/cyberhus_clean/js/scroll-to.js
+++ sites/all/themes/cyberhus_clean/js/scroll-to.js
@@ -1,5 +1,7 @@
 $ = jQuery;
 
+// Scroll to top
+
 var scrollTopOffset = 100; // Offset
 var scrollBottomOffset = 100; // Offset
 
@@ -36,5 +38,25 @@ $(document).ready(function() {
 $(window).on('scroll', function () {
 
   backToTopInit();
+
+});
+
+
+// Scroll to Related Content
+
+$(document).ready(function() {
+
+  if ($('.related-link').length) {
+
+    var relatedOffset = $('#related-content').offset();
+
+    $('.related-link').on('click', function (e) {
+      e.preventDefault();
+
+      $('html,body').animate({
+        scrollTop: relatedOffset.top
+      }, 750);
+    });
+  }
 
 });
