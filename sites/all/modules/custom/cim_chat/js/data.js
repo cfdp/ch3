1ee398d79f4298b9816d02242c800d52ce54cdaf
Benjamin Christensen <udvikling@greenvolcano.dk>
2020-02-04 10:17:29 +0100

    Fix malfunctioning button logic
    Use safe strings for css classes in buttons

--- sites/all/modules/custom/cim_chat/js/data.js
+++ sites/all/modules/custom/cim_chat/js/data.js
@@ -1,56 +1,67 @@
 var cimChats = { 
-  ng9Nhlvt5HpKIbqm6l66xzK05WAw3Our: {
+  'ng9Nhlvt5HpKIbqm6l66xzK05WAw3Our': {
     shortName: 'Middelfart',
+    cssClassName: 'cim-btn-middelfart',
     domLocation: '.municipality-chats',
     chatServerURL: 'https://chat.ecmr.biz/ChatClient/'
   },
-  G3jlsM1ldCxKIbqm6l66xzK05WAw3Our: {
+  'G3jlsM1ldCxKIbqm6l66xzK05WAw3Our': {
     shortName: 'Fredensborg',
+    cssClassName: 'cim-btn-fredensborg',
     domLocation: '.municipality-chats',
     chatServerURL: 'https://chat.ecmr.biz/ChatClient/'
   },
-  MwBzImS0nr9KIbqm6l66xzK05WAw3Our: {
+  'MwBzImS0nr9KIbqm6l66xzK05WAw3Our': {
     shortName: 'Stevns',
+    cssClassName: 'cim-btn-stevns',
     domLocation: '.municipality-chats',
     chatServerURL: 'https://chat.ecmr.biz/ChatClient/'
   },
-  L3FaKitRcLpKIbqm6l66xzK05WAw3Our: {
+  'L3FaKitRcLpKIbqm6l66xzK05WAw3Our': {
     shortName: 'Slagelse',
+    cssClassName: 'cim-btn-slagelse',
     domLocation: '.municipality-chats',
     chatServerURL: 'https://chat.ecmr.biz/ChatClient/'
   },
   '68tZgzarWkdKIbqm6l66xzK05WAw3Our': {
     shortName: 'Aabenraa',
+    cssClassName: 'cim-btn-aabenraa',
     domLocation: '.municipality-chats',
     chatServerURL: 'https://chat.ecmr.biz/ChatClient/'
   },
   'pRhGYvy2K4tKIbqm6l66xzK05WAw3Our': {
     shortName: 'Skanderborg',
+    cssClassName: 'cim-btn-skanderborg',
     domLocation: '.municipality-chats',
     chatServerURL: 'https://chat.ecmr.biz/ChatClient/'
   },
   'J3zZCN8hejFKIbqm6l66xzK05WAw3Our': {
     shortName: 'Odder',
+    cssClassName: 'cim-btn-odder',
     domLocation: '.municipality-chats',
     chatServerURL: 'https://chat.ecmr.biz/ChatClient/'
   },
   '7qU6QkLyKrxKIbqm6l66xzK05WAw3Our': {
     shortName: 'Odense',
+    cssClassName: 'cim-btn-odense',
     domLocation: '.municipality-chats',
     chatServerURL: 'https://chat.ecmr.biz/ChatClient/'
   },
   'vD2iSGhX+phKIbqm6l66xzK05WAw3Our': {
     shortName: 'Silkeborg',
+    cssClassName: 'cim-btn-silkeborg',
     domLocation: '.municipality-chats',
     chatServerURL: 'https://chat.ecmr.biz/ChatClient/'
   },
   'ELyzJV0fvhZKIbqm6l66xzK05WAw3Our': {
     shortName: 'Cyberhus',
+    cssClassName: 'cim-btn-cyberhus',
     domLocation: '.cyberhus-chats',
     chatServerURL: 'https://chat.ecmr.biz/ChatClient/'
   },
   'H6bj65Uxut1KIbqm6l66xzK05WAw3Our': {
     shortName: 'Aarhus',
+    cssClassName: 'cim-btn-aarhus',
     domLocation: '.municipality-chats',
     chatServerURL: 'https://chat.ecmr.biz/ChatClient/'
   }
