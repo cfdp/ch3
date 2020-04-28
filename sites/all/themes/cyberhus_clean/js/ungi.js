ff11125c4dd3b4df2221b1d78e836dc8db5108c3
Benjamin Christensen <udvikling@greenvolcano.dk>
2020-04-28 11:50:33 +0200

    More specific removal of bg

--- sites/all/themes/cyberhus_clean/js/ungi.js
+++ sites/all/themes/cyberhus_clean/js/ungi.js
@@ -4,9 +4,10 @@ $(document).ready(function() {
 
     $(".wide").parents('.field-item').addClass("wide-element");
     $(".narrow").parents('.field-item').addClass("narrow-element");
-    // Don't add bg class to elements integrating the cim chat
-    if (!$(".paragraphs-item-ungi-alm-tekst").hasClass('cim-chat-embedded')) {
-        $(".paragraphs-item-ungi-alm-tekst").parent('.field-item').addClass("add-bg");
+    $(".paragraphs-item-ungi-alm-tekst").parents('.field-item').addClass("add-bg");
+    // Remove bg class to elements integrating the cim chat
+    if ($(".paragraphs-item-ungi-alm-tekst").hasClass('cim-chat-embedded')) {
+        $(".paragraphs-item-ungi-alm-tekst").parent('.field-item').removeClass("add-bg");
     };
     $(".paragraphs-item-ungi-lokale-tilbud").parents('.field-item').addClass("add-bg");
     $(".paragraphs-item-ungi-iframe-embed").parents('.field-item').addClass("add-bg grey");
