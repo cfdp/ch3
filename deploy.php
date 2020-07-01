93b1b76127b6964b6dbe77942f0b67cb6c8ffbad
Anita Jensen <ahj@novicell.dk>
2020-07-01 14:36:32 +0200

    updating deployer

--- deploy.php
+++ deploy.php
@@ -1,9 +1,15 @@
 <?php
 namespace Deployer;
 
+$config_file = 'deployer/config.yml';
+if (!is_file($config_file) || !is_readable($config_file)) {
+  echo 'Could not find configuration file';
+  exit;
+}
+
 // Include recipes
 require 'deployer/recipe/base.php';
 
 // Load config file
-inventory('deployer/config.yml');
+inventory($config_file);
 
